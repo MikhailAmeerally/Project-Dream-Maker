2ca03ab5bc53ce97f4000e362c6994b887ad7361
Mikhail Ameerally <MAmeerally@indigo.ca>
2018-09-26 13:32:35 -0400

    Basic multi-equation calculator working

---
+++ Content/MiniProjects/Calculator/functions.py
@@ -0,0 +1,31 @@
+def printHelp():
+    print("Example phrase: 3 + 23")
+    print("Example phrase: 45 * 234")
+    print("Rules need to be followed.")
+
+
+def add(a,b):
+    return a + b
+
+
+def subtract(a,b):
+    return a-b
+
+
+def divide(a,b):
+    return a / b
+
+
+def  multiply(a,b):
+    return a * b
+
+
+def evaluate(a, b, operator):
+    if operator == "+":
+        return add(a,b)
+    if operator == "-":
+        return subtract(a,b)
+    if operator == "*":
+        return multiply(a,b)
+    if operator == "/":
+        return divide(a,b)
--- Content/MiniProjects/Calculator/main.py
+++ Content/MiniProjects/Calculator/main.py
@@ -1,2 +1,29 @@
-print("Hello,  I am a Calculator.")
-print("Can you help make me?")
+from functions import evaluate
+
+print("This calculator only uses + - * /")
+print("You are more than welcomed to make it more advanced.")
+print("For help, type 'help'")
+
+while(True):
+    phrase = input("Enter phrase: ")
+    if phrase.strip() == "help":
+        printHelp()
+    elif phrase == "exit":
+        break
+    else:
+        phrase = phrase.strip().split()
+        a = None
+        b = None
+        operator = None
+        aFlag = True
+        for i in phrase:
+            if i not in ['+', '-', '*', '/']:
+                if aFlag:
+                    a = int(i)
+                    aFlag = False
+                else:
+                    b = int(i)
+                    a = evaluate(a, b, operator)
+            else:
+                operator = i
+        print(a)
